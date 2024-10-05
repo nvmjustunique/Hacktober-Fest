b557d24163ff09c52de04f89af1bfef44b61bda7
nvmjustunique <adityaharbude2006@gmail.com>
2024-10-05 12:31:41 +0530

    13.py
    easy python
    debugged

--- Python/Easy/13.py
+++ Python/Easy/13.py
@@ -1,4 +1,4 @@
-time = 5
+t = 5
 g = 9.8
 distance = g * t ** 2  # Error: 't' is not defined, typo in variable name
 print("Distance traveled in free fall:", distance)
--- Python/Easy/8.py
+++ Python/Easy/8.py
@@ -1,9 +1,13 @@
-if x = 5:
+
+X = 5
+if X == 5:
+
+
     print("x is 5")
-if x == 5:
+if X== 5:
     x = 10
     print("x is now 10")
 
 x = 5
-if x = 5:
+if x == 5:
     print("x is 5")
